59ede6ac5fd8ab7a96a4e3a0e82460091a36c7f6
VincentAdamNemessis <126950990+VincentAdamNemessisX@users.noreply.github.com>
2024-05-21 12:48:54 +0800

    Update app.py
    Remove some contents that not needed.

--- app.py
+++ app.py
@@ -3,10 +3,5 @@ from function import app
 app = app
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2222)
+    app.run()
